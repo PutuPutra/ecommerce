fd6c8b5950ed97f809112b1a675dae2372f40def
Putu Putra Eka Persada <putupersada@gmail.com>
2025-03-19 13:22:05 +0700

    Fix formatting in RootLayout component by ensuring consistent spacing after Footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -8,7 +8,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 flex flex-col p-4">{children}</main>
-      <Footer />
+      <Footer /> 
     </div>
   );
 }
